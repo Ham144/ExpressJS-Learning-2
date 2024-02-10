9925ee489a8e81a7f282ae1240607a3207ca9610
ham144 <24434muhammad.yafizham@gmail.com>
2024-02-11 01:39:57 +0700

    hihihi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { query } from 'express-validator'
 
 const Port = process.env.PORT || 3000
 const app = express()
@@ -12,6 +13,19 @@ const users = [
     { id: 4, userName: "riska", age: 23 },
 ]
 
+const resolveUsersById = (req, res, next) => {
+    const { id } = req.params
+    if (id > users.length) return res.status(404).send(`Data Dengan id: ${id} dangadong`)
+    const parsedId = parseInt(id)
+    if (isNaN(parsedId)) return res.status(402)
+    const foundIndex = users.findIndex((user) => {
+        return user.id === parsedId
+    })
+    if (foundIndex === -1) return res.status(404).send("Data tidak deketemukan")
+    req.foundIndex = foundIndex
+    next()
+}
+
 
 app.post('/users', (req, res) => {
     const { body } = req
@@ -21,18 +35,11 @@ app.post('/users', (req, res) => {
 })
 
 
-app.put('/users/:id', (req, res) => {
-    const { body, params: { id } } = req;
-    const parsedId = parseInt(id)
-    if (isNaN(parsedId)) return res.status(402).send("Params harus angka")
-    const findUser = users.findIndex((user) => user.id === parsedId)
-    if (findUser === -1) return res.sendStatus(404)
-    users[findUser] = { id: parsedId, ...body }
+app.put('/users/:id', resolveUsersById, (req, res) => {
+    const { body, foundIndex } = req;
+    users[foundIndex] = { id: users[foundIndex].id, ...body }
     return res.status(200)
 })
-
-
-
 
 
 app.get('/users', (req, res) => {
@@ -58,41 +65,23 @@ app.get('/users', (req, res) => {
 
 
 
-app.get('/users/:id', (req, res) => {
-    const parseId = parseInt(req.params.id)
-    if (isNaN(parseId)) return res.status(404).send(`<h1>ID should be Number</h1>`);
-
-    const findUser = users.find((user) => (
-        user.id === parseId  //INGAT HARUS PAKAI RETURN UNTUK Fungsi array syarat/kriteria atau bungkus ()
-    ));
-    if (!findUser) return res.sendStatus(401)
-    return res.send(findUser)
+app.get('/users/:id', resolveUsersById, (req, res) => {
+    const { foundIndex } = req
+    const user = users[foundIndex]
+    return res.send(user)
 })
 
-app.patch("/users/:id", (req,res) =>{
-    const {params : {id}, body } = req
-    const parsedId = parseInt(id)
-    if(isNaN(parsedId)) return req.status(405).send(`<h1>ID harus angka </h1>`)
-    const foundIndex = users.findIndex((user) => {
-
-        return user.id === parsedId
-    })
-    if(foundIndex === -1) return res.status(404).send(`<h1>Not found with the id ${parsedId}</h1>`)
-    users[foundIndex] = {...users[foundIndex], ...body}
+app.patch("/users/:id", resolveUsersById, (req, res) => {
+    const { foundIndex, body } = req
+    users[foundIndex] = { ...users[foundIndex], ...body }
     return res.status(202).send(users)
 })
 
 
-app.delete('/users', (req,res) => {
-    const {params : {id}} = req
-    const parsedId = parseInt(id)
-    if(isNaN(parsedId)) return res.status(402).send("ID must be A number")
-    const foundIndex = users.findIndex((user) => {
-        return user.id === parsedId
-     })
-    if(foundIndex === -1) return res.status(404).send("not Found bro") 
+app.delete('/users/:id', resolveUsersById, (req, res) => {
+    const { foundIndex } = req
     users.splice(foundIndex, 1)
-    res.status(202)
+    return res.status(202)
 })
 
 
